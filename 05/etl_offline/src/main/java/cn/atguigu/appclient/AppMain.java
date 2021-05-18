a6b0d59a4bdb3f1d1e5abc34ced0108312a253b9
ming <ming@qq.com>
2021-05-18 16:55:49 +0800

    2021年5月18日

---
+++ 05/etl_offline/src/main/java/cn/atguigu/appclient/AppMain.java
@@ -0,0 +1,11 @@
+package cn.atguigu.appclient;
+
+/**
+ * TODO
+ *
+ * @author ming
+ * @date 2021/5/18 16:42
+ */
+public class AppMain {
+
+}
---
+++ 05/etl_offline/src/main/java/cn/atguigu/bean/AppBase.java
@@ -0,0 +1,21 @@
+package cn.atguigu.bean;
+
+/**
+ * TODO
+ *公共日志
+ * @author ming
+ * @date 2021/5/18 16:44
+ */
+public class AppBase {
+    private String mid; //设备唯一标识
+    private String uid; //用户uid
+    private String vc; //versionCode程序版本号
+    private String vn; //versionName程序版本名
+    private String l; //系统语言
+    private String sr; //渠道号，source
+    private String os; //android系统版本
+    private String ar; //区域
+    private String md; //手机型号
+    private String ba; //手机品牌
+    private String sv;
+}
